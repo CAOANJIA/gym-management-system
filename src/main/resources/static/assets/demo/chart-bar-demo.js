1a1bbbaa2a0c5abec57b5635345d49070d970e02
王一楠 <1341457359@qq.com>
2022-09-05 17:29:56 +0800

    修改会员、教练管理界面字段，微改了下图标名称

--- src/main/resources/static/assets/demo/chart-bar-demo.js
+++ src/main/resources/static/assets/demo/chart-bar-demo.js
@@ -9,10 +9,10 @@ var myLineChart = new Chart(ctx, {
   data: {
     labels: ["January", "February", "March", "April", "May", "June"],
     datasets: [{
-      label: "Revenue",
+      label: "Members",
       backgroundColor: "rgba(2,117,216,1)",
       borderColor: "rgba(2,117,216,1)",
-      data: [4215, 5312, 6251, 7841, 9821, 14984],
+      data: [4, 5, 10, 17, 21, 30],
     }],
   },
   options: {
@@ -31,7 +31,7 @@ var myLineChart = new Chart(ctx, {
       yAxes: [{
         ticks: {
           min: 0,
-          max: 15000,
+          max: 100,
           maxTicksLimit: 5
         },
         gridLines: {
